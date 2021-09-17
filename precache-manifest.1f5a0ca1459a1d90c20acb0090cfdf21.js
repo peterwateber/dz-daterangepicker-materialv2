0dec8dd6608262507d03af66675ea80a0e6e99f0
Peter <peterwateber@gmail.com>
2021-09-17 20:18:13 +0200

    Updates

---
+++ precache-manifest.1f5a0ca1459a1d90c20acb0090cfdf21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc914826c485f43523f5497467ccf006",
+    "url": "/dz-daterangepicker-materialv2/index.html"
+  },
+  {
+    "revision": "9a899f0ed92231570e6d",
+    "url": "/dz-daterangepicker-materialv2/static/css/main.748229e6.chunk.css"
+  },
+  {
+    "revision": "ade0e1c24c185dd3c501",
+    "url": "/dz-daterangepicker-materialv2/static/js/2.6dc7639a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dz-daterangepicker-materialv2/static/js/2.6dc7639a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a899f0ed92231570e6d",
+    "url": "/dz-daterangepicker-materialv2/static/js/main.322b72f8.chunk.js"
+  },
+  {
+    "revision": "c8c0bec15e5ae630205a",
+    "url": "/dz-daterangepicker-materialv2/static/js/runtime-main.e9ed602b.js"
+  }
+]);
